2e072c4763da6229f2469c1126174838e86fd317
Felipe Santos <136188197+Felipe8959@users.noreply.github.com>
2024-12-30 14:40:41 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from imblearn.pipeline import Pipeline  # Use o pipeline do imblearn
+from imblearn.over_sampling import SMOTE
 
+# Atualize o pipeline para incluir o SMOTE
 pipeline = Pipeline([
-    ("tfidf", TfidfVectorizer(
+    ('tfidf', TfidfVectorizer(
         ngram_range=(1, 2),
         max_df=0.9,
-        min_df=2,
         max_features=5000,
-        sublinear_tf=True,
-        # Caso deseje testar remoção de stopwords:
-        # stop_words='portuguese'
+        sublinear_tf=True
     )),
-    ("clf", LogisticRegression(
+    ('smote', SMOTE(sampling_strategy='auto', random_state=42)),  # Aplica o SMOTE
+    ('lr', LogisticRegression(
         penalty='l2',
-        C=1.0,               # regularização mediana
-        solver='lbfgs',
-        random_state=42,
-        class_weight='balanced'  # útil se houver desbalanceamento
+        C=1.0,  # Regularização
+        random_state=6162,
+        class_weight='balanced'
     ))
 ])
+
+# Ajuste o pipeline usando os dados de treino
+pipeline.fit(train_data_pd['clean_text'], train_data_pd['ANALISE_RESPOSTA_1'])
+
+# Faça previsões
+test_data_pd['predictions'] = pipeline.predict(test_data_pd['clean_text'])
+test_data_pd['probabilities'] = pipeline.predict_proba(test_data_pd['clean_text'])[:, 1]
